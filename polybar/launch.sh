a6dd1b00bd8e8399b55a5dcaf34fa339597620fa
Linh Nguyen <linh+github@dmesg.sh>
2021-09-28 18:48:45 +0700

    use env variable to config network module

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,11 +11,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
+itf=$(ip a | awk '/state UP/ {print $2}' | cut -d ":" -f1)
 number_of_monitor=$(($(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)))
 if [ $number_of_monitor -gt 1 ]; then
   for m in $(xrandr --query | grep -v eDP | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main -c "$DIR/config.ini" &
+    NETWORK_ITF=$itf MONITOR=$m polybar --reload main -c "$DIR/config.ini" &
   done
 else
-  polybar --reload main -c "$DIR/config.ini" &
+  NETWORK_ITF=$itf polybar --reload main -c "$DIR/config.ini" &
 fi
